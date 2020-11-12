5a368424e38dd9349d6b5229df7dfa103aa6f03a
sdutwsl <wanghuilongsd@163.com>
2020-11-12 23:50:51 +0800

    重新设置了config.json配置文件并读入

--- server/index.js
+++ server/index.js
@@ -1,12 +1,20 @@
 const axios = require("axios").default
+const fs=require("fs")
+
+const ali_config=JSON.parse(fs.readFileSync("config.json").toString())
 
 // 万年历接口
-const wnl_api=" https://jisuwnl.market.alicloudapi.com/calendar/"
+const wnl_api="https://jisuwnl.market.alicloudapi.com/calendar/"
 // 天气接口
 const weather_api="https://saweather.market.alicloudapi.com/area-to-weather"
 
+
 async function get_lunnar_data(api) {
-    axios.get(api,{
-        
-    })
+    const res=axios({
+        method: 'get',
+        url: wnl_api,
+        data: {
+            headers: {'Authorization': 'XMLHttpRequest'},
+        }
+      })
 }
