5daf121ecd11e7c5f7be0900246de077e60f977e
sdutwsl <wanghuilongsd@163.com>
2020-11-12 23:33:48 +0800

    增加了万年历和天气接口

--- server/index.js
+++ server/index.js
@@ -1,1 +1,7 @@
-console.log("This is server")
+const axios = require("axios")
+
+// 万年历接口
+const wnl_api=" https://jisuwnl.market.alicloudapi.com/calendar/"
+// 天气接口
+const weather_api="https://saweather.market.alicloudapi.com/area-to-weather"
+
