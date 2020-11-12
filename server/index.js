52394012220e00cb3341064d60ab6f46ab0ebcc0
sdutwsl <wanghuilongsd@163.com>
2020-11-12 23:41:59 +0800

    修改config.json为config.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
-const axios = require("axios")
+const axios = require("axios").default
 
 // 万年历接口
 const wnl_api=" https://jisuwnl.market.alicloudapi.com/calendar/"
 // 天气接口
 const weather_api="https://saweather.market.alicloudapi.com/area-to-weather"
 
+async function get_lunnar_data(api) {
+    axios.get(api,{
+        
+    })
+}
