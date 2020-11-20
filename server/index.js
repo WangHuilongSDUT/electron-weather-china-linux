c24ced70fb96d727edeea60dab8f87b4e375803f
sdutwsl <wanghuilongsd@163.com>
2020-11-20 18:00:38 +0800

    修改了res 不能修改的bug

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const ali_config = JSON.parse(fs.readFileSync("config.json").toString())
 async function get_lunnar_data() {
     const now = new Date()
     //使用aliyun市场的万年历接口
-    const res=null
+    let res=null
     try {
         res = await axios.get("https://jisuwnl.market.alicloudapi.com/calendar/" + `query?date=`, { headers: { 'Authorization': 'APPCODE ' + ali_config["app_code"] } })
     } catch (e) {
@@ -22,7 +22,7 @@ async function get_lunnar_data() {
 
 // 获取24小时天气
 async function get_weather24_data(place = "闵行区") {
-    const res=null
+    let res=null
     try {
         res = await axios.get("http://saweather.market.alicloudapi.com/hour24?area=" + encodeURI(place), { headers: { 'Authorization': 'APPCODE ' + ali_config["app_code"] } })
     } catch (e) {
@@ -33,7 +33,7 @@ async function get_weather24_data(place = "闵行区") {
 
 //获取每周天气
 async function get_weather7_data(place = "闵行区") {
-    const res=null
+    let res=null
     try {
         res = await axios.get(`https://saweather.market.alicloudapi.com/area-to-weather?area=${encodeURI(place)}&needAlarm=1&needIndex=1&needMoreDay=1`, { headers: { 'Authorization': 'APPCODE ' + ali_config["app_code"] } })
     } catch (e) {
