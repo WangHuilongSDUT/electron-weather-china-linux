cf10e1751d1370b7896c476dd17e51cccb90a693
sdutwsl <wanghuilongsd@163.com>
2020-11-20 16:28:44 +0800

    增加了天气查询函数

--- server/index.js
+++ server/index.js
@@ -3,14 +3,28 @@ const fs=require("fs")
 
 const ali_config=JSON.parse(fs.readFileSync("config.json").toString())
 
-// 万年历接口
-const wnl_api="https://jisuwnl.market.alicloudapi.com/calendar/"
-// 天气接口
-const weather_api="https://saweather.market.alicloudapi.com/area-to-weather"
+/****************************************************** 基本功能函数 ******************************************************************* */
 
-
-async function get_lunnar_data(api=wnl_api) {
+// 获取万年历
+async function get_lunnar_data() {
     const now=new Date()
-    const res=await axios.get(api+`query?date=`,{headers: {'Authorization': 'APPCODE '+ali_config["app_code"]}})
+    //使用aliyun市场的万年历接口
+    const res=await axios.get("https://jisuwnl.market.alicloudapi.com/calendar/"+`query?date=`,{headers: {'Authorization': 'APPCODE '+ali_config["app_code"]}})
     return res.data
 }
+
+// 获取24小时天气
+async function get_weather24_data(place="闵行区"){
+    const res=await axios.get("http://saweather.market.alicloudapi.com/hour24?area="+encodeURI(place),{headers: {'Authorization': 'APPCODE '+ali_config["app_code"]}})
+    return res.data
+}
+
+//获取每周天气
+async function get_weather7_data(place="闵行区"){
+    const res=await axios.get(`https://saweather.market.alicloudapi.com/area-to-weather?area=${encodeURI(place)}&needAlarm=1&needIndex=1&needMoreDay=1`,{headers: {'Authorization': 'APPCODE '+ali_config["app_code"]}})
+    return res.data
+}
+
+
+/**************************************************** 服务器内容 ************************************************************************************* */
+
